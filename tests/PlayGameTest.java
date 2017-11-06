88f6ad952757b8cc4357b6ec7375209a6f22976a
jspr <33236346+jspr@users.noreply.github.com>
2017-11-06 10:44:43 +0100

    REFACTOR

--- tests/PlayGameTest.java
+++ tests/PlayGameTest.java
@@ -15,9 +15,14 @@ import view.IView;
 public class PlayGameTest {
 	
 	private PlayGame sut;
+	IView view;
+	SlotMachine slotMachine;
 
 	@Before
 	public void setUp() throws Exception {
+		sut = new PlayGame();
+		view = mock(EnglishView.class);
+		slotMachine = new SlotMachine();
 	}
 
 	@After
@@ -26,18 +31,12 @@ public class PlayGameTest {
 
 	@Test
 	public void shouldDisplayWelcomeMessage() {
-		sut = new PlayGame();
-		IView view = mock(EnglishView.class);
-		SlotMachine slotMachine = new SlotMachine();
 		sut.play(slotMachine, view);
 		verify(view).displayWelcomeMessage();
 	}
 	
 	@Test
 	public void shouldDisplayInstructions() {
-		sut = new PlayGame();
-		IView view = mock(EnglishView.class);
-		SlotMachine slotMachine = new SlotMachine();
 		sut.play(slotMachine, view);
 		verify(view).displayInstructions();
 	}
