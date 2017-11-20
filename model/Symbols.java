67188fe655fa7ac5bee3a7b0e25604a5c3c7723d
jspr <33236346+jspr@users.noreply.github.com>
2017-11-20 18:07:03 +0100

    GREEN

--- model/Symbols.java
+++ model/Symbols.java
@@ -1,15 +1,24 @@
 package model;
 
 public enum Symbols {
-	SYMBOL_1,
-	SYMBOL_2,
-	SYMBOL_3,
-	SYMBOL_4,
-	SYMBOL_5,
-	SYMBOL_6,
-	SYMBOL_7,
-	SYMBOL_8,
-	SYMBOL_9,
-	SYMBOL_10;
+	SYMBOL_1("000"),
+	SYMBOL_2("001"),
+	SYMBOL_3("010"),
+	SYMBOL_4("011"),
+	SYMBOL_5("100"),
+	SYMBOL_6("101"),
+	SYMBOL_7("110"),
+	SYMBOL_8("111"),
+	SYMBOL_9("FOO"),
+	SYMBOL_10("BAR");
 	
+	private final String symbolString;
+	
+	Symbols(String symbolString) {
+      this.symbolString = symbolString;
+	}
+	
+	public String toString() {
+		return symbolString;
+	}
 }
