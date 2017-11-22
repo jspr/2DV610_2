d294ffd02f4fda1226d3e13d5e47671eb4302d34
jspr <33236346+jspr@users.noreply.github.com>
2017-11-22 20:53:31 +0100

    RED

---
+++ tests/ReelTest.java
@@ -0,0 +1,31 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import model.Reel;
+import model.SlotMachine;
+import model.Symbols;
+
+public class ReelTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void shouldReturnString000() {
+		Reel sut = new Reel();
+		assertEquals("000", sut.getSymbol());
+	}
+	
+
+
+}
