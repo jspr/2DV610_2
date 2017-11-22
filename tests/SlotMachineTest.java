cb5a134e37827f03eb2564840172f44700cff350
jspr <33236346+jspr@users.noreply.github.com>
2017-11-22 15:22:38 +0100

    RED

--- tests/SlotMachineTest.java
+++ tests/SlotMachineTest.java
@@ -1,12 +1,16 @@
 package tests;
 
 import static org.junit.Assert.*;
+import static org.mockito.Mockito.mock;
+
+import java.util.Collection;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
 import model.SlotMachine;
+import model.Symbols;
 
 public class SlotMachineTest {
 	
@@ -56,6 +60,35 @@ public class SlotMachineTest {
 		int expected = 3;
 		int actual = sut.getSymbols().size();
 		assertEquals(expected,actual);
+	}
+	
+	@Test
+	public void shouldReturnArrayWithStrings_000_000_000() {
+		sut = new SlotMachine(1,3,10,3);
+		String expected = "000";
+		Collection<String> symbolStrings = sut.getSymbols();
+		assertEquals(expected,symbolStrings.toArray()[0]);
+		assertEquals(expected,symbolStrings.toArray()[1]);
+		assertEquals(expected,symbolStrings.toArray()[2]);
+	}
+	
+	@Test
+	public void shouldReturnArrayWithStrings_BAR_BAR_BAR() {
+		sut = new SlotMachine(1,3,10,3);
+		String expected = "BAR";
+		Collection<String> symbolStrings = sut.getSymbols();
+		assertEquals(expected,symbolStrings.toArray()[0]);
+		assertEquals(expected,symbolStrings.toArray()[1]);
+		assertEquals(expected,symbolStrings.toArray()[2]);
+	}
+	
+	@Test
+	public void shouldReturnArrayWithStrings_000_FOO_BAR() {
+		sut = new SlotMachine(1,3,10,3);
+		Collection<String> symbolStrings = sut.getSymbols();
+		assertEquals("000",symbolStrings.toArray()[0]);
+		assertEquals("FOO",symbolStrings.toArray()[1]);
+		assertEquals("BAR",symbolStrings.toArray()[2]);
 	}
 
 }
